f9c0f71b265d0c6a41d2e706c27553180642efb1
wenchao <860694896@qq.com>
2020-11-23 17:16:36 +0800

    保存数据到txt文件中

--- news/pipelines.py
+++ news/pipelines.py
@@ -11,15 +11,36 @@ from scrapy.exporters import CsvItemExporter
 
 class NewsPipeline:
 
+    # def __init__(self):
+    #     self.file = open('news_data.csv', 'wb')
+    #     self.exporter = CsvItemExporter(self.file, encoding='utf-8')
+    #     self.exporter.start_exporting()
+    #
+    # def close_spider(self, spider):
+    #     self.exporter.finish_exporting()
+    #     self.file.close()
+    #
+    # def process_item(self, item, spider):
+    #     self.exporter.export_item(item)
+    #     return item
+
     def __init__(self):
-        self.file = open('news_data.csv', 'wb')
-        self.exporter = CsvItemExporter(self.file, encoding='utf-8')
-        self.exporter.start_exporting()
+        self.file = open('news_data.txt', 'w')
 
     def close_spider(self, spider):
-        self.exporter.finish_exporting()
         self.file.close()
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
+        self.file.write(item['news_title'])
+        self.file.write('\n')
+        self.file.write(item['news_time'])
+        self.file.write('  ')
+        self.file.write('来源：' + item['news_source'])
+        self.file.write('\n')
+        contents = item['news_body']
+        for content in contents:
+            self.file.write('   ' + content.replace('\n', '').replace(' ', ''))
+            self.file.write('\n')
+        self.file.write('*' * 50)
+        self.file.write('\n')
         return item
